175e7b6455dac28dc2876066e1f0f7d6be1dbb59
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2025-02-06 16:40:12 -0500

    add upload functionality

--- src/pages/Datasync.tsx
+++ src/pages/Datasync.tsx
@@ -1,10 +1,26 @@
 import { Button, Typography } from 'antd';
 import { DownloadOutlined, PlusOutlined } from '@ant-design/icons';
 import DatasyncTable from '../components/DatasyncTable';
+import DatasyncSource from '../interface/model/DatasyncSource';
+import { useRef, useState } from 'react';
 
 const { Title } = Typography;
 
 const Datasync = () => {
+  const [uploadData, setUploadData] = useState<DatasyncSource[] | null>(null);
+  const [files, setFiles] = useState<File[]>([]);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFiles(Array.from(event.target.files));
+    }
+  };
+
   return (
     <div>
       <div className="m-10">
@@ -13,7 +29,15 @@ const Datasync = () => {
             Data Sync
           </Title>
           <div>
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              ref={fileInputRef}
+              style={{ display: 'none' }}
+            />
             <Button
+              onClick={handleButtonClick}
               type="primary"
               shape="round"
               className="mr-2"
@@ -22,7 +46,7 @@ const Datasync = () => {
             <Button type="primary" shape="round" icon={<DownloadOutlined />} />
           </div>
         </div>
-        <DatasyncTable dataSource={null} />
+        <DatasyncTable dataSource={uploadData} />
       </div>
     </div>
   );
