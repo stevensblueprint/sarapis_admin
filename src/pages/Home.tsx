e8f804f38397711763a5af9a46fd990032d5a11c
Vinayak Ranjan <njvinny@gmail.com>
2025-02-12 16:42:59 -0500

    Applied prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,30 @@
-import { Card } from "antd";
-import { HomeOutlined, DollarOutlined, 
-    BookOutlined, ShoppingOutlined, CoffeeOutlined, 
-    CarOutlined, MedicineBoxOutlined, BankOutlined, 
-    AlertOutlined, HeartOutlined, IdcardOutlined } from "@ant-design/icons";
+import { Card } from 'antd';
+import {
+  HomeOutlined,
+  DollarOutlined,
+  BookOutlined,
+  ShoppingOutlined,
+  CoffeeOutlined,
+  CarOutlined,
+  MedicineBoxOutlined,
+  BankOutlined,
+  AlertOutlined,
+  HeartOutlined,
+  IdcardOutlined,
+} from '@ant-design/icons';
 
 const categories = [
-    { title: "Care", icon: <HeartOutlined className="text-5xl" /> },
-    { title: "Emergency", icon: <AlertOutlined className="text-5xl" /> },
-    { title: "Goods", icon: <ShoppingOutlined className="text-5xl" /> },
-    { title: "Housing", icon: <HomeOutlined className="text-5xl" /> },
-    { title: "Money", icon: <DollarOutlined className="text-5xl" /> },
-    { title: "Work", icon: <IdcardOutlined className="text-5xl" /> },
-    { title: "Education", icon: <BookOutlined className="text-5xl" /> },
-    { title: "Food", icon: <CoffeeOutlined className="text-5xl" />},
-    { title: "Health", icon: <MedicineBoxOutlined className="text-5xl" /> },
-    { title: "Legal", icon: <BankOutlined className="text-5xl" /> },
-    { title: "Transit", icon: <CarOutlined className="text-5xl" /> },
+  { title: 'Care', icon: <HeartOutlined className="text-5xl" /> },
+  { title: 'Emergency', icon: <AlertOutlined className="text-5xl" /> },
+  { title: 'Goods', icon: <ShoppingOutlined className="text-5xl" /> },
+  { title: 'Housing', icon: <HomeOutlined className="text-5xl" /> },
+  { title: 'Money', icon: <DollarOutlined className="text-5xl" /> },
+  { title: 'Work', icon: <IdcardOutlined className="text-5xl" /> },
+  { title: 'Education', icon: <BookOutlined className="text-5xl" /> },
+  { title: 'Food', icon: <CoffeeOutlined className="text-5xl" /> },
+  { title: 'Health', icon: <MedicineBoxOutlined className="text-5xl" /> },
+  { title: 'Legal', icon: <BankOutlined className="text-5xl" /> },
+  { title: 'Transit', icon: <CarOutlined className="text-5xl" /> },
 ];
 
 const full_rows = 5 * Math.floor(categories.length / 5);
@@ -27,7 +36,9 @@ const Home = () => {
       <div className="grid grid-cols-5 gap-6">
         {categories.slice(0, full_rows).map((category, index) => (
           <Card key={index} className="w-40 h-40 flex-col text-center">
-            <div className="flex justify-center items-center">{category.icon}</div>
+            <div className="flex justify-center items-center">
+              {category.icon}
+            </div>
             <p className="text-lg font-medium mt-2">{category.title}</p>
           </Card>
         ))}
@@ -35,7 +46,9 @@ const Home = () => {
       <div className="grid grid-cols-auto gap-6 mt-6">
         {categories.slice(full_rows).map((category, index) => (
           <Card key={index} className="w-40 h-40 flex-col text-center">
-            <div className="flex justify-center items-center">{category.icon}</div>
+            <div className="flex justify-center items-center">
+              {category.icon}
+            </div>
             <p className="text-lg font-medium mt-2">{category.title}</p>
           </Card>
         ))}
