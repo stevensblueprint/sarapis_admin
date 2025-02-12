d244d50fef2d7557182566abde30e1c5b003bfcb
Jonathan Memoli <jonomemoli3@gmail.com>
2025-02-12 15:02:47 -0500

    Map Pins (#9)
    * Change map component to render pins of service locations
    
    * Change popups to work on hover and click
    
    * Prettier

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,25 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Service } from '../interface/model/Service';
+import { useState } from 'react';
 
-const Map = () => {
+interface MapProps {
+  services: Service[];
+}
+interface MapInfo {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+const Map = ({ services }: MapProps) => {
+  const [popupClicked, setPopupClicked] = useState<string | null>(null);
+
+  const mapInfoList: MapInfo[] = services.flatMap((service) =>
+    service.serviceAtLocations.map((serviceAtLocation) => {
+      const { id, name, latitude, longitude } = serviceAtLocation.location;
+      return { id, name, latitude, longitude };
+    })
+  );
   return (
     <div
       style={{ height: '100vh', borderRadius: '15px', overflow: 'hidden' }}
@@ -16,6 +35,33 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {mapInfoList.map((location: MapInfo) => (
+          <Marker
+            key={location.id}
+            position={[location.latitude, location.longitude]}
+            eventHandlers={{
+              mouseover: (event) => {
+                event.target.openPopup();
+              },
+              mouseout: (event) => {
+                if (popupClicked !== location.id) {
+                  event.target.closePopup();
+                }
+              },
+              click: () => {
+                setPopupClicked(location.id);
+              },
+            }}
+          >
+            <Popup
+              eventHandlers={{
+                remove: () => setPopupClicked(null),
+              }}
+            >
+              Name: {location.name}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
