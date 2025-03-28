068ba825628885607fd6d21cf02865f7afe0434a
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2025-03-28 18:50:49 -0400

    implement add all button to table selection, store current states of time range and table selection

--- src/components/ExportModal.tsx
+++ src/components/ExportModal.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react';
-import { Button, Typography, Modal, DatePicker } from 'antd';
+import { Button, Typography, Modal, DatePicker, Select } from 'antd';
 import {
   DownloadOutlined,
   LoadingOutlined,
   CheckOutlined,
   WarningOutlined,
+  CloseOutlined,
 } from '@ant-design/icons';
 import { getAllFiles } from '../api/lib/datasync';
+import type { SelectProps } from 'antd';
 
 const { Title } = Typography;
 const { RangePicker } = DatePicker;
 
 type DownloadStatus = 'IDLE' | 'DOWNLOADING' | 'DONE' | 'ERROR';
+
+const tableOptions: SelectProps['options'] = [
+  { label: 'Option 1', value: 'option1' },
+  { label: 'Option 2', value: 'option2' },
+  { label: 'Option 3', value: 'option3' },
+];
 
 const ExportModal = ({
   showModal,
@@ -22,6 +30,9 @@ const ExportModal = ({
 }) => {
   const [dateRange, setDateRange] = useState<[string, string] | null>(null);
   const [downloadStatus, setDownloadStatus] = useState<DownloadStatus>('IDLE');
+  const [selectedTableOptions, setSelectedTableOptions] = useState<
+    (string | number | null | undefined)[]
+  >([]);
 
   useEffect(() => {
     if (showModal) {
@@ -31,7 +42,12 @@ const ExportModal = ({
 
   const handleDateChange = (dates, dateString: [string, string]) => {
     setDateRange(dateString);
-    console.log(dateString);
+  };
+
+  const handleTableSelectChange = (
+    selection: (string | number | null | undefined)[]
+  ) => {
+    setSelectedTableOptions(selection);
   };
 
   const getButtonIcon = () => {
@@ -49,6 +65,7 @@ const ExportModal = ({
 
   const getFiles = async () => {
     try {
+      console.log(selectedTableOptions);
       setDownloadStatus('DOWNLOADING');
       const response = await getAllFiles({
         format: 'PDF',
@@ -82,7 +99,7 @@ const ExportModal = ({
       <Modal
         open={showModal}
         onCancel={closeModal}
-        title="Export All Data"
+        title="Export Data"
         footer={
           <Button
             type="primary"
@@ -95,11 +112,37 @@ const ExportModal = ({
         }
       >
         <div className="p-8">
-          <Title level={4}>Time Range</Title>
-          <RangePicker format="MMMM DD, YYYY" onChange={handleDateChange} />
-          <Title level={4} className="mt-4">
-            Tables
-          </Title>
+          <Title level={3}>Time Range</Title>
+          <RangePicker
+            className="w-[100%]"
+            format="MMMM DD, YYYY"
+            onChange={handleDateChange}
+          />
+          <div>
+            <Title level={3} className="mt-8">
+              Tables
+            </Title>
+          </div>
+          <div className="flex flex-row gap-2">
+            <Select
+              className="w-[100%]"
+              mode="multiple"
+              placeholder="Select Tables..."
+              options={tableOptions}
+              onChange={handleTableSelectChange}
+              value={selectedTableOptions}
+              allowClear
+            />
+            <Button
+              onClick={() =>
+                handleTableSelectChange(
+                  tableOptions.map((option) => option.value)
+                )
+              }
+            >
+              Add All
+            </Button>
+          </div>
         </div>
       </Modal>
     </div>
