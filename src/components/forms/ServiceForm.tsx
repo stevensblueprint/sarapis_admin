fbbf95795676225b24509bfb9eb9a5b35f9417b3
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2025-04-29 15:56:08 -0400

    move basic form tab to separate component, create one form component for all steps

--- src/components/forms/ServiceForm.tsx
+++ src/components/forms/ServiceForm.tsx
@@ -1,24 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Button, Form, Input, Modal, Select, Steps } from 'antd';
-import Response from '../../interface/Response';
-import Organization from '../../interface/model/Organization';
-import { getAllOrganizations } from '../../api/lib/organizations';
-import CollapsibleFormSelectTable from '../CollapsibleFormSelectTable';
-import RequiredDocument from '../../interface/model/RequiredDocument';
-import CollapsibleFormTable from '../CollapsibleFormTable';
-import RequiredDocumentForm from './RequiredDocumentsForm';
-import { requiredDocumentsTableColumns } from '../../data/RequiredDocumentsData';
-import Phone from '../../interface/model/Phone';
-import Contact from '../../interface/model/Contact';
-import LocationForm from './LocationForm';
-import { locationTableColumns } from '../../data/LocationData';
-import PhoneForm from './PhoneForm';
-import { phoneTableColumns } from '../../data/PhoneData';
-import ContactForm from './ContactForm';
-import { contactTableColumns } from '../../data/ContactData';
-import Location from '../../interface/model/Location';
-import RegularScheduleForm from './RegularScheduleForm';
-import HolidayScheduleForm from './HolidayScheduleForm';
+import { Button, Form, Modal, Steps } from 'antd';
+import BasicForm from './BasicForm';
+
+const { Step } = Steps;
 
 const ServiceForm = ({
   showModal,
@@ -29,283 +13,24 @@ const ServiceForm = ({
 }) => {
   const [showServiceModal, setShowServiceModal] = useState<boolean>(false);
   const [currentStep, setCurrentStep] = useState<number>(0);
-  const [organizations, setOrganizations] = useState<Organization[]>([]);
-  const [requiredDocuments, setRequiredDocuments] = useState<
-    RequiredDocument[]
-  >([]);
-  const [locations, setLocations] = useState<Location[]>([]);
-  const [phones, setPhones] = useState<Phone[]>([]);
-  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [stepData, setStepData] = useState({
+    step0: {},
+    step1: {},
+  });
   const [form] = Form.useForm();
 
   useEffect(() => {
     setShowServiceModal(showModal);
   }, [showModal]);
 
-  useEffect(() => {
-    const fetchOrganizations = async () => {
-      try {
-        const response = await getAllOrganizations();
-        const data = response.data as Response<Organization[]>;
-        setOrganizations(data.contents || []);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchOrganizations();
-  }, []);
-
   const formSteps = [
     {
       title: 'Basic',
-      content: (
-        <div className="self-center md:w-2/3 lg:w-1/2">
-          <Form form={form} variant="filled" size="large">
-            <Form.Item
-              label="Service Name"
-              name="Service Name"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Organization Name"
-              name="Organization Name"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Select
-                showSearch
-                placeholder="Select an Organization"
-                options={organizations.map((organization) => {
-                  return { value: organization.id, label: organization.name };
-                })}
-              />
-            </Form.Item>
-            <Form.Item
-              label="Service Description"
-              name="Service Description"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item
-              label="Service URL (website)"
-              name="Service URL"
-              rules={[
-                { required: true },
-                { type: 'url', warningOnly: true },
-                { type: 'string', min: 6 },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Service Email"
-              name="Service Email"
-              rules={[
-                { required: true },
-                { type: 'email', warningOnly: true },
-                { type: 'string', min: 6 },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Service Area"
-              name="Service Area"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Select />
-            </Form.Item>
-            <Form.Item
-              label="Service Alternate Name"
-              name="Service Alternate Name"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Accreditations"
-              name="Accreditations"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Funding"
-              name="Funding"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input />
-            </Form.Item>
-          </Form>
-        </div>
-      ),
+      content: <BasicForm />,
     },
     {
-      title: 'Language',
-      content: (
-        <div className="self-center md:w-2/3 lg:w-1/2">
-          <Form form={form} variant="filled">
-            <Form.Item
-              label="Languages"
-              name="languages"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Select
-                mode="multiple"
-                options={[{ value: '1', label: 'English' }]}
-              />
-            </Form.Item>
-            <Form.Item
-              label="Interpretation Services"
-              name="Interpretation Services"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item
-              label="Service Alert"
-              name="Service Alert"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-          </Form>
-        </div>
-      ),
-    },
-    {
-      title: 'Application',
-      content: (
-        <div className="self-center md:w-2/3 lg:w-1/2">
-          <Form form={form} variant="filled">
-            <Form.Item
-              label="Application Process"
-              name="Application Process"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item
-              label="Fee Description"
-              name="Fee Description"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item
-              label="Eligibility Description"
-              name="Eligibility Description"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item
-              label="Eligibility Requirement"
-              name="Eligibility Requirement"
-              rules={[{ required: true, message: 'Required field!' }]}
-            >
-              <Select
-                options={[
-                  { value: '1', label: 'None' },
-                  { value: '2', label: 'Yes' },
-                ]}
-              />
-            </Form.Item>
-          </Form>
-        </div>
-      ),
-    },
-    {
-      title: 'Documents',
-      content: (
-        <CollapsibleFormTable
-          formLabel="Add a new Required Document"
-          customForm={
-            <RequiredDocumentForm
-              parentForm={form}
-              setRequiredDocument={setRequiredDocuments}
-            />
-          }
-          emptyText="Required Documents"
-          tableColumns={requiredDocumentsTableColumns || []}
-          dataSource={requiredDocuments}
-        />
-      ),
-    },
-    {
-      title: 'Location',
-      content: (
-        <CollapsibleFormSelectTable
-          formLabel="Add a new Location"
-          selectLabel="Select an existing Location"
-          customForm={
-            <LocationForm parentForm={form} setLocations={setLocations} />
-          }
-          parentForm={form}
-          dropdownLabel="Location Name"
-          dropdownName="location Name"
-          dropdownPlaceholder="Select a Location"
-          emptyText="Locations"
-          options={locations.map((location) => {
-            return { value: location.id, label: location.name };
-          })}
-          tableColumns={locationTableColumns || []}
-          dataSource={locations}
-        />
-      ),
-    },
-    {
-      title: 'Phone',
-      content: (
-        <CollapsibleFormSelectTable
-          formLabel="Add a new Phone"
-          selectLabel="Select an existing Phone"
-          customForm={<PhoneForm parentForm={form} setPhones={setPhones} />}
-          parentForm={form}
-          dropdownLabel="Phone Number"
-          dropdownName="Phone Number"
-          dropdownPlaceholder="Select a Phone"
-          emptyText="Phones"
-          options={phones.map((phone) => {
-            return { value: phone.id, label: phone.number };
-          })}
-          tableColumns={phoneTableColumns || []}
-          dataSource={phones}
-        />
-      ),
-    },
-    {
-      title: 'Contact',
-      content: (
-        <CollapsibleFormSelectTable
-          formLabel="Add a new Contact"
-          selectLabel="Select an existing Contact"
-          customForm={
-            <ContactForm parentForm={form} setContacts={setContacts} />
-          }
-          parentForm={form}
-          dropdownLabel="Contact Name"
-          dropdownName="Contact Name"
-          dropdownPlaceholder="Select a Contact"
-          emptyText="Contacts"
-          options={contacts.map((contact) => {
-            return { value: contact.id, label: contact.name };
-          })}
-          tableColumns={contactTableColumns || []}
-          dataSource={contacts}
-        />
-      ),
-    },
-    {
-      title: 'Schedule',
-      content: (
-        <div>
-          <RegularScheduleForm parentForm={form} />
-          <HolidayScheduleForm />
-        </div>
-      ),
+      title: 'Basic2',
+      content: <BasicForm />,
     },
   ];
 
@@ -358,9 +83,11 @@ const ServiceForm = ({
 
   const handleCancel = () => {
     setShowServiceModal(false);
-    setCurrentStep(7);
+    setCurrentStep(0);
     closeModal();
   };
+
+  const onFinish = () => {};
 
   return (
     <Modal
@@ -371,14 +98,38 @@ const ServiceForm = ({
       centered
       width={'80%'}
     >
-      <div className="flex flex-col align-center gap-10">
-        <div>
-          <Steps
-            current={currentStep}
-            items={formSteps.map((step) => ({ title: step.title }))}
-          />
-        </div>
-        {formSteps[currentStep].content}
+      <div>
+        <Form
+          className="flex flex-col align-center gap-10"
+          form={form}
+          layout="vertical"
+          initialValues={stepData['step0'] || {}}
+        >
+          <Steps current={currentStep} style={{ marginBottom: 24 }}>
+            {formSteps.map((s) => (
+              <Step key={s.title} title={s.title} />
+            ))}
+          </Steps>
+
+          {formSteps[currentStep].content}
+
+          <div style={{ marginTop: 24 }}>
+            {currentStep > 0 && (
+              <Button onClick={prev} style={{ marginRight: 8 }}>
+                Previous
+              </Button>
+            )}
+            {currentStep < formSteps.length - 1 ? (
+              <Button type="primary" onClick={next}>
+                Next
+              </Button>
+            ) : (
+              <Button type="primary" onClick={onFinish}>
+                Submit
+              </Button>
+            )}
+          </div>
+        </Form>
       </div>
     </Modal>
   );
