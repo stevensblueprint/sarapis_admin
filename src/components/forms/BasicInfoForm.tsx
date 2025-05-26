ace85f16942acbc5f72a69e7335cc6114f9282e1
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2025-05-26 00:53:28 -0700

    make organization required field

--- src/components/forms/BasicInfoForm.tsx
+++ src/components/forms/BasicInfoForm.tsx
@@ -118,6 +118,7 @@ const BasicInfoForm = ({ form }: { form: FormInstance }) => {
               </div>
             }
             name="organization"
+            rules={[{ required: true, message: 'Required Field!' }]}
           >
             {selectedOrganization ? (
               <div className="flex flex-row items-center gap-2">
